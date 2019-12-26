083fb6a0364ba1aef6ba1809ab308cb625585ee9
z1078274519 <1078274519@qq.com>
2019-12-26 21:48:29 +0800

    Initial commit

---
+++ src/main/java/com/zhoujin/springbootdataredis/SpringbootDataRedisApplication.java
@@ -0,0 +1,13 @@
+package com.zhoujin.springbootdataredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDataRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDataRedisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhoujin/springbootdataredis/SpringbootDataRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhoujin.springbootdataredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDataRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
